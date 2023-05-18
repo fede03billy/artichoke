5e57bdd85b84c7105e404e1c0b2ec3954f4d6191
Federico C <fede03billy@gmail.com>
2023-05-18 15:05:57 +0200

    Added some text and url parsing

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import requests
+import re
+from urllib.parse import urlparse
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 
@@ -17,7 +19,32 @@ def scrape_article():
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
 
-    scraped_data = {"title": soup.title.string, "url": url, "text": soup.get_text()}
+    text = soup.get_text()
+    # Replace newline and carriage return with space
+    text = text.replace("\n", " ").replace("\r", " ").replace("\t", " ")
+    # Remove leading/trailing white spaces
+    text = text.strip()
+    text = re.sub(" +", " ", text)
+
+    main_image = soup.find(
+        "img"
+    )  # pass {"class": "class-name"} as second argument to find specific image
+    image_url = (
+        main_image["src"] if main_image else "https://via.placeholder.com/600x300"
+    )
+
+    parsed_url = urlparse(url)
+    domain = parsed_url.scheme + "://" + parsed_url.netloc
+    # check if image url begin with the domain or https (and another domain)
+    if not image_url.startswith(domain) and not image_url.startswith("https"):
+        image_url = domain + image_url
+
+    scraped_data = {
+        "title": soup.title.string,
+        "url": url,
+        "text": text,
+        "image": image_url,
+    }
 
     return jsonify(scraped_data)
 
